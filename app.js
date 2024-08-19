d7fdc0e669805cdcde34d40efe74da19d126f9da
Juan Manuel Adame <juanmadametor@gmail.com>
2024-08-19 15:44:49 +0200

    Cambios de diseño
    He cambiado el diseño en distintos apartados

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ const icon= document.querySelector(".hamburger-icon");
 menu.classList.toggle("open")
 icon.classList.toggle("open")
 }
+
+
+const logos=document.querySelectorAll(".logo")
+
+
+logos.forEach(logo=>{
+    logo.addEventListener('click',scroll)
+})
+
+function scroll(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+}
