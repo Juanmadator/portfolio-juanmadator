3d0a3a0476cb0c331f20ec1a6135e3c8cdf2480d
Juan Manuel Adame <juanmadametor@gmail.com>
2024-08-22 02:26:16 +0200

    Avances tech e inicio
    Avances y cambios

--- app.js
+++ app.js
@@ -29,3 +29,27 @@ document.querySelectorAll('.flip-card').forEach(card => {
         this.classList.toggle('clicked');
     });
 });
+
+
+  // Función para manejar el scroll
+  function handleScroll() {
+    const profile = document.querySelector('#profile');
+    const about = document.querySelector('#about');
+    const profileRect = profile.getBoundingClientRect();
+    const aboutRect = about.getBoundingClientRect();
+
+    // Verifica si el usuario ha hecho scroll hacia la sección de "about"
+    if (aboutRect.top <= window.innerHeight && aboutRect.bottom >= 0) {
+        profile.classList.add('hidden');
+        about.classList.add('profile-3d');
+    } else {
+        profile.classList.remove('hidden');
+        about.classList.remove('profile-3d');
+    }
+}
+
+// Añadir un evento para escuchar el scroll
+window.addEventListener('scroll', handleScroll);
+
+// Llamar a la función al cargar la página por si se encuentra en la sección al inicio
+handleScroll();
